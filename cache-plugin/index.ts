d5b66c855acc837b12e6cabd299bbb16be84cd14
branberry <brandon.ly@mongodb.com>
2024-06-10 10:11:47 -0500

    Add create cache files

--- cache-plugin/index.ts
+++ cache-plugin/index.ts
@@ -1,3 +1,11 @@
-export function onSuccess() {
-  console.log('onSuccess custom plugin logs')
+import { execFile } from 'child_process';
+
+import { promisify } from 'util';
+
+const execFileAsync = promisify(execFile)
+
+export async function onSuccess() {
+  console.log('Creating cache files...')
+  await execFileAsync('./snooty-parser/snooty/snooty', ['create-cache', '.'])
+  console.log('Cache files created')
 }
