6cf6ce3ca5e44a844e4e86798b6c5a970ba64a0b
branberry <brandon.ly@mongodb.com>
2024-08-12 11:05:38 -0500

    Add logging

--- netlify/edge-functions/redirects.ts
+++ netlify/edge-functions/redirects.ts
@@ -5,22 +5,24 @@ const SITES: Record<string, string> = {
 const HOST_PATTERN = /^\/docs\/([^\/]+)\/([^\/]+)(.+)/;
 const NOT_FOUND_URL = new URL("https://www.mongodb.com/docs/404/");
 
-export default async (req: Request): Promise<Response> => {
+export default async (req: Request): Promise<Response | URL> => {
   const requestUrl = new URL(req.url);
 
   // Look at the request path and deduce what host we want to route to
   const match = requestUrl.pathname.match(HOST_PATTERN);
   if (match === null) {
     console.error(`Error with url: ${req.url}: Not fully qualified`);
-    return fetch(NOT_FOUND_URL);
+    return requestUrl;
   }
   const [_, sitename, siteversion, newpath] = match;
 
 
-  console.log(newpath)
   // Construct a new URL to return
   const hostname = `${siteversion}--docs-node-branfork.netlify.app`;
   const url = new URL(`https://${hostname}`);
+
+  console.log('url', url);
+  url.pathname = newpath;
 
   return fetch(url);
 };
