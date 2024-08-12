3c0426b7de5728388d7ba3c67b7d00c1edf8a985
branberry <brandon.ly@mongodb.com>
2024-08-12 11:22:57 -0500

    update regex

--- netlify/edge-functions/redirects.ts
+++ netlify/edge-functions/redirects.ts
@@ -2,7 +2,7 @@ const SITES: Record<string, string> = {
   "kubernetes-operator": "docs-k8s-operator",
 };
 
-const HOST_PATTERN = /^\/docs\/([^\/]+)\/([^\/]+)(.+)/;
+const HOST_PATTERN = /(?:v\d\.\d+)/;
 const NOT_FOUND_URL = new URL("https://www.mongodb.com/docs/404/");
 
 export default async (req: Request): Promise<Response | undefined> => {
@@ -11,18 +11,17 @@ export default async (req: Request): Promise<Response | undefined> => {
   // Look at the request path and deduce what host we want to route to
   const match = requestUrl.pathname.match(HOST_PATTERN);
   if (match === null) {
-    console.error(`Error with url: ${req.url}: Not fully qualified`);
     return;
   }
   const [_, sitename, siteversion, newpath] = match;
-
+  console.log('match', match)
 
   // Construct a new URL to return
   const hostname = `${siteversion}--docs-node-branfork.netlify.app`;
   const url = new URL(`https://${hostname}`);
 
   console.log('url', url);
-  url.pathname = newpath;
+  // url.pathname = newpath;
 
   return fetch(url);
 };
