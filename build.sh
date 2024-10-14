c3a020d714c4e2faa1207ab0cd23bf17fed6dd4d
Brandon Ly <brandonly@lostcoding.com>
2024-10-14 07:59:45 -0500

    Update build.sh

--- build.sh
+++ build.sh
@@ -19,7 +19,7 @@ echo "==========================================================================
 
 if [ ! -d "snooty" ]; then
   echo "snooty frontend not installed, downloading"
-  git clone -b netlify-poc --depth 1 https://github.com/mongodb/snooty.git 
+  git clone -b 2d492e22fba4b3624773c10c413a4603c42a05b1 --depth 1 https://github.com/mongodb/snooty.git 
   echo GATSBY_MANIFEST_PATH=$(pwd)/bundle.zip >> ./snooty/.env.production
   cd snooty
   npm ci --legacy-peer-deps
