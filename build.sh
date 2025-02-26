e7b295849040a6f4cdc10677349a2665ca1e78d1
branberry <brandon.ly@mongodb.com>
2025-02-26 12:46:38 -0600

    add quotes

--- build.sh
+++ build.sh
@@ -21,7 +21,7 @@ if [ ! -d "snooty" ]; then
   echo "snooty frontend not installed, downloading"
   git clone -b netlify-poc --depth 1 https://github.com/mongodb/snooty.git 
   echo GATSBY_MANIFEST_PATH=$(pwd)/bundle.zip >> ./snooty/.env.production
-    echo PATH_PREFIX=/ >> ./snooty/.env.production
+  echo PATH_PREFIX="/" >> ./snooty/.env.production
 
   cd snooty
   npm ci --legacy-peer-deps
